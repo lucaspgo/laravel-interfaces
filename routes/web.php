4386438c0400d42cded46a13d14ac174acb27ce4
Lucas Pereira Gonçalves de Oliveira <lucas.oliveira@madeiramadeira.com.br>
2024-06-04 19:21:58 -0300

    Cria Rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/reset', '')->name('reset');
+Route::get('/balance', '')->name('balance');
+Route::post('/event', '')->name('event');
