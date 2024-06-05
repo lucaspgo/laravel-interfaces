13f6b1684932977d7bc2835f263c4441b85deea0
Lucas Pereira Gonçalves de Oliveira <lucas.oliveira@madeiramadeira.com.br>
2024-06-04 21:03:07 -0300

    Correct to Accounts

--- database/migrations/2024_06_04_234123_create_account_table.php
+++ database/migrations/2024_06_04_234123_create_account_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('account', function (Blueprint $table) {
+        Schema::create('accounts', function (Blueprint $table) {
             $table->id();
             $table->double('balance');
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('account');
+        Schema::dropIfExists('accounts');
     }
 };
