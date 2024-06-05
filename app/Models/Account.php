649460d8017cbd61ed9d92548bf1e75f5462771e
Lucas Pereira Gonçalves de Oliveira <lucas.oliveira@madeiramadeira.com.br>
2024-06-05 08:06:53 -0300

    Deposit Function

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\JsonResponse;
 
 /**
  * @property int $id
@@ -13,8 +14,28 @@ class Account extends Model
 {
     use HasFactory;
 
-    public function create(){}
-    public function deposit(){}
-    public function withdraw(){}
+    public const CREATED_AT = 'dt_inc';
+    public const UPDATED_AT = 'dt_alt';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'balance'
+    ];
+
+    /**
+     * Deposit or Create an Account.
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function deposit($request){
+        $this->id = $request->destination;
+        $this->balance = $this->balance + $request->amount;
+        $this->save();
+
+        return response()->json(['destination' => ['id' => $this->id, 'balance' => $this->balance]]);
+    }
+    public function withdraw(){
+    }
     public function transfer(){}
 }
