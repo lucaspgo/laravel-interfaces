e37db37162c000bab4856198b6353dfc4aba0c47
Lucas Pereira Gonçalves de Oliveira <lucas.oliveira@madeiramadeira.com.br>
2024-06-05 08:09:00 -0300

    Update Contoller Routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Http\Middleware\CustomMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/reset', '')->name('reset');
-Route::get('/balance', '')->name('balance');
-Route::post('/event', '')->name('event');
+Route::get('/balance', 'App\Http\Controllers\AccountController@balance')->name('balance');
+Route::post('/event', 'App\Http\Controllers\AccountController@event')->name('event');
+// Route::post('/reset', '')->name('reset');
+
