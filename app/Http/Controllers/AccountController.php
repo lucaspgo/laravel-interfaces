984d31d9093be0e67915ecb85fb4791484051029
Lucas Pereira Gonçalves de Oliveira <lucas.oliveira@madeiramadeira.com.br>
2024-06-09 22:30:48 -0300

    Término das construção das funções da Controller

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,27 +2,69 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Account;
+use App\Interfaces\AccountInterface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
+    public function __construct(public AccountInterface $account)
+    {
+    }
+
     /**
-     * Returns Balance.
+     * Delete all data from Account
+     *
+     * @return JsonResponse
+     */
+    public function reset(): JsonResponse
+    {
+        $this->account->reset();
+
+        return response()->json("OK", 200);
+    }
+
+    /**
+     * Get balance of account.
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
      */
     public function balance(Request $request): JsonResponse
     {
-        return Account::find($request->account_id) != null ?
-            response()->json(Account::find($request->id)->balance, 200) :
+        /** @var Account */
+        $account = $this->account->getById($request->account_id);
+
+        return $account->id ?
+            response()->json($account->balance, 200) :
             response()->json(0, 404);
     }
 
     /**
-     * Returns Balance.
+     * Performs deposit, withdrawal, and transfer transactions on accounts.
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
      */
     public function event(Request $request): JsonResponse
     {
-        return Account::find($request->destination) ? Account::find($request->destination)->deposit($request) : (new Account())->deposit($request);
+        switch ($request->type) {
+            case 'deposit':
+                return response()->json(
+                    $this->account->deposit($request),
+                        201);
+            case 'withdraw':
+                return $this->account->getById($request->origin)->id ?
+                    response()->json($this->account->withdraw($request), 201) :
+                    response()->json(0, 404);
+            case 'transfer':
+                return !$this->account->getById($request->origin)->id ?
+                    response()->json(0, 404) :
+                    response()->json($this->account->transfer($request), 201);
+            default:
+                return response()->json(0, 500);
+        }
     }
 }
