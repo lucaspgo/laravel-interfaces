ceefe842b3167d55c582a86e6ba00ea0092f9d90
Lucas Pereira Gonçalves de Oliveira <lucas.oliveira@madeiramadeira.com.br>
2024-06-09 22:30:22 -0300

    Adiciona a Interface e Service nos Providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\AccountInterface;
+use App\Services\AccountService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(AccountInterface::class, AccountService::class);
     }
 
     /**
